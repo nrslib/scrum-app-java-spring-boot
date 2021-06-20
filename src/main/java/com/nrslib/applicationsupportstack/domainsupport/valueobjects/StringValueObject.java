3370e527e0ae59b590b393d09a17b644e96c2c23
nrslib <masanobu.naruse@gmail.com>
2021-06-20 16:06:50 +0900

    implement equals and hashcode

--- src/main/java/com/nrslib/applicationsupportstack/domainsupport/valueobjects/StringValueObject.java
+++ src/main/java/com/nrslib/applicationsupportstack/domainsupport/valueobjects/StringValueObject.java
@@ -2,11 +2,26 @@ package com.nrslib.applicationsupportstack.domainsupport.valueobjects;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 public class StringValueObject {
     @Getter
     private final String value;
 
     public StringValueObject(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StringValueObject that = (StringValueObject) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
